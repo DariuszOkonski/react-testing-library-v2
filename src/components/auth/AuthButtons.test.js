a9db9b26b205771cc03b351af5bfba834c192c32
Dariusz Okoński <darek200180@gmail.com>
2024-02-14 19:28:26 +0100

    add describe

--- src/components/auth/AuthButtons.test.js
+++ src/components/auth/AuthButtons.test.js
@@ -3,12 +3,40 @@ import { MemoryRouter } from 'react-router-dom';
 import { createServer } from '../../test/server';
 import AuthButtons from './AuthButtons';
 
-// createServer() ===> GET '/api/user' ===> { user: null }
-test('when user is not signed in, sign in and sign up are visible', async () => {});
+describe('when user is not signed in', () => {
+  // createServer() ===> GET '/api/user' ===> { user: null }
+  createServer([
+    {
+      path: '/api/user',
+      res: () => {
+        return { user: null };
+      },
+    },
+  ]);
+  test('when user is not signed in, sign in and sign up are visible', async () => {
+    console.log('test 1');
+  });
 
-test.skip('when user is not signed in, sign up is not visible', async () => {});
+  test('when user is not signed in, sign up is not visible', async () => {
+    console.log('test 2');
+  });
+});
 
-// createServer() ===> GET '/api/user' ===> { id: 3, user: 'test1@gmail.com' }
-test.skip('when user is signed in, sign in and sign up are not visible', async () => {});
+describe('when user is signed in', () => {
+  // createServer() ===> GET '/api/user' ===> { id: 3, user: 'test1@gmail.com' }
+  createServer([
+    {
+      path: '/api/user',
+      res: () => {
+        return { user: { id: 2, email: 'test1@gmail.com' } };
+      },
+    },
+  ]);
+  test('when user is signed in, sign in and sign up are not visible', async () => {
+    console.log('test 3');
+  });
 
-test.skip('when user is signed in, sign out is visible', async () => {});
+  test('when user is signed in, sign out is visible', async () => {
+    console.log('test 4');
+  });
+});
