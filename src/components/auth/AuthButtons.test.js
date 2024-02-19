6d750c7d7cb98bffb60a176b3162ee4026cbf023
Dariusz Okoński <darek200180@gmail.com>
2024-02-19 19:26:27 +0100

    add additional test

--- src/components/auth/AuthButtons.test.js
+++ src/components/auth/AuthButtons.test.js
@@ -60,11 +60,28 @@ describe('when user is signed in', () => {
     },
   ]);
   test('sign in and sign up are not visible', async () => {
-    renderComponent();
+    await renderComponent();
+
+    const signInButton = screen.queryByRole('link', {
+      name: /sign in/i
+    });
+    const signUpButton = screen.queryByRole('link', {
+      name: /sign up/i
+    });
+
+    expect(signInButton).not.toBeInTheDocument();
+    expect(signUpButton).not.toBeInTheDocument();
   });
 
   test('sign out is visible', async () => {
-    renderComponent();
+    await renderComponent();
+
+    const signOutButton = screen.getByRole('link', {
+      name: /sign out/i
+    })
+
+    expect(signOutButton).toBeInTheDocument();
+    expect(signOutButton).toHaveAttribute('href', '/signout')
   });
 });
 
